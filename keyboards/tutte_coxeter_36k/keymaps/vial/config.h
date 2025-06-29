98706fef99f5a5e9408305c70b5138105e32f4ec
Peter J. A. Cock <p.j.a.cock@googlemail.com>
2025-06-29 11:20:13 +0900

    Try less layers since compilation error hints at dynamic limits
    Was getting: Dynamic keymaps are configured to use more EEPROM than is available.

--- keyboards/tutte_coxeter_36k/keymaps/vial/config.h
+++ keyboards/tutte_coxeter_36k/keymaps/vial/config.h
@@ -3,7 +3,7 @@
 /* Randomly generated as per Vial docs */
 #define VIAL_KEYBOARD_UID {0x1C, 0xF4, 0xA2, 0x03, 0x11, 0x30, 0xC7, 0x49}
 
-#define DYNAMIC_KEYMAP_LAYER_COUNT 8
+#define DYNAMIC_KEYMAP_LAYER_COUNT 4
 
 /* Original Gamma-Omega used keys 0,0 (Q) and 3,6 (P) */
 /* with our sparse matrix those are 1,0 (Q) and 12,11 (P) instead. */
