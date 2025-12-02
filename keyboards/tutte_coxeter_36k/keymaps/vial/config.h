3f4027d930691675f240824d18dd1da574ddf5bd
Peter Cock <p.j.a.cock@googlemail.com>
2025-12-02 01:17:27 +0000

    Update TC36K Vial unlock combo
    Forgot this with the re-ordering to get Q at 0,0 in matrix.

--- keyboards/tutte_coxeter_36k/keymaps/vial/config.h
+++ keyboards/tutte_coxeter_36k/keymaps/vial/config.h
@@ -5,7 +5,7 @@
 
 #define DYNAMIC_KEYMAP_LAYER_COUNT 6
 
-/* Original Gamma-Omega used keys 0,0 (Q) and 3,6 (P) */
-/* with our sparse matrix those are 12,11 (Q) and 1,0 (P) instead. */
-#define VIAL_UNLOCK_COMBO_ROWS { 12, 1 }
-#define VIAL_UNLOCK_COMBO_COLS { 11, 0 }
+/* Original Gamma-Omega used top left (Q) and top right (P) */
+/* with our sparse matrix those are 0,0 (Q) and 1,12 (P) instead. */
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 1, 12 }
