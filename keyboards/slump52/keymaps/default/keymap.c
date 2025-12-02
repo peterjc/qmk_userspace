a5abbf45bdbae2cec184969d7a267bb33bd9989b
Peter J. A. Cock <p.j.a.cock@googlemail.com>
2025-12-02 02:04:20 +0000

    Try to build Slump52 QMK/Vial firmware on GitHub Actions

--- keyboards/slump52/keymaps/default/keymap.c
+++ keyboards/slump52/keymaps/default/keymap.c
@@ -20,10 +20,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       * ASDF are Left-GUI,    Left-Alt,  Left-Ctrl, Left-Shift
       * JKL- are Right-Shift, Left-Ctrl, Left-Alt,  Left-GUI
       */
-    [0] = LAYOUT(
-        KC_ESC, KC_Q,               KC_W,               KC_E,               KC_R,               KC_T,      KC_Y,     KC_U,               KC_I,               KC_O,                KC_P,                  KP_EQUAL, KP_7, KP_8, KP_9,
-                MT(MOD_LGUI, KC_A), MT(MOD_LALT, KC_S), MT(MOD_LCTL, KC_D), MT(MOD_LSFT, KC_F), KC_G,      KC_H,     MT(MOD_RSFT, KC_J), MT(MOD_LCTL, KC_K), MT(MOD_LALT, KC_L),  MT(MOD_LGUI, KC_MINUS),          KP_4, KP_5, KP_6,
-                KC_Z,               KC_X,               KC_C,               KC_V,               KC_B,      KC_N,     KC_M,               KC_COMM,            KC_DOT,              KC_SLSH,                         KP_1, KP_2, KP_3,
-                KC_TAB,                                                                    KC_BSPC, KC_LSHFT,  KC_SPC,       KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT,                   KC_KP_ASTERISK,                  KC_0,    KC_ENT
+    [0] = LAYOUT_default(
+        KC_ESC, KC_Q,               KC_W,               KC_E,               KC_R,               KC_T,      KC_Y,     KC_U,               KC_I,               KC_O,                KC_P,                  KC_EQUAL, KC_7, KC_8, KC_9,
+                MT(MOD_LGUI, KC_A), MT(MOD_LALT, KC_S), MT(MOD_LCTL, KC_D), MT(MOD_LSFT, KC_F), KC_G,      KC_H,     MT(MOD_RSFT, KC_J), MT(MOD_LCTL, KC_K), MT(MOD_LALT, KC_L),  MT(MOD_LGUI, KC_MINUS),          KC_4, KC_5, KC_6,
+                KC_Z,               KC_X,               KC_C,               KC_V,               KC_B,      KC_N,     KC_M,               KC_COMM,            KC_DOT,              KC_SLSH,                         KC_1, KC_2, KC_3,
+                KC_TAB,                                                                    KC_BSPC, KC_LSFT, KC_SPC,         KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT,                   KC_KP_ASTERISK,                  KC_0,    KC_ENT
     )
 };
