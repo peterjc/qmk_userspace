53d8ccea5c84a3f9cb4abf3cafee0d99d3915a28
Peter J. A. Cock <p.j.a.cock@googlemail.com>
2026-01-10 11:20:46 +0000

    QMK Bivvy16D firmware
    Did ZMK DTSI to KLE JSON with https://shield-wizard.genteure.workers.dev/
    
    QMK views the pair as having double the rows instead of double the columns.
    ZMK can do either, but I had been doing double the cols.
    
    Setting SERIAL_DRIVER = vendor for RPi's usart, design allows 4 wire
    TRRS with full duplex https://docs.qmk.fm/drivers/serial#usart-full-duplex

---
+++ keyboards/bivvy16d/keymaps/vial/config.h
@@ -0,0 +1,10 @@
+#pragma once
+
+/* Randomly generated as per Vial docs */
+#define VIAL_KEYBOARD_UID {0x1C, 0xF4, 0xA2, 0x03, 0x11, 0x30, 0xC7, 0x51}
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 6
+
+/* Top left key at matrix 0,0 (Q) and top right of left hand at matrix 0,4 (T) */
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 4 }
