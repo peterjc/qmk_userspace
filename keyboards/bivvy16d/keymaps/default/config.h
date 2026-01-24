a1492bd5dad74ad6faeb264ed6ac749298f3e60e
Peter Cock <p.j.a.cock@googlemail.com>
2026-01-24 22:20:02 +0000

    SERIAL_USART_FULL_DUPLEX in  config.h

--- keyboards/bivvy16d/keymaps/default/config.h
+++ keyboards/bivvy16d/keymaps/default/config.h
@@ -1,5 +1,8 @@
 #pragma once
 
+/* Enable full duplex operation mode */
+#define SERIAL_USART_FULL_DUPLEX
+
 /* The GPIO pins used for full duplex split communication */
 #define SERIAL_USART_TX_PIN GP12
 #define SERIAL_USART_RX_PIN GP13
