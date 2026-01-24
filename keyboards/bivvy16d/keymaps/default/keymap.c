d18594a1ccb36097f98ead2dacba054732f745d1
Peter Cock <p.j.a.cock@googlemail.com>
2026-01-24 22:09:03 +0000

    Fix KC_PGUP typo in keymap.c

--- keyboards/bivvy16d/keymaps/default/keymap.c
+++ keyboards/bivvy16d/keymaps/default/keymap.c
@@ -33,7 +33,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_Q,               KC_W,               KC_E,               KC_R,               KC_T,      KC_Y,     KC_U,               KC_I,               KC_O,                KC_P,
         MT(MOD_LGUI, KC_A), MT(MOD_LALT, KC_S), MT(MOD_LCTL, KC_D), MT(MOD_LSFT, KC_F), KC_G,      KC_H,     MT(MOD_RSFT, KC_J), MT(MOD_LCTL, KC_K), MT(MOD_LALT, KC_L),  MT(MOD_LGUI, KC_SCLN),
         KC_Z,               KC_X,               KC_C,               KC_V,               KC_B,      KC_N,     KC_M,               KC_COMM,            KC_DOT,              KC_SLSH,
-        KC_PGDN,            KC_HOME,            KC_KP_ENTER,        KC_END,             KP_PGUP,   KC_UP,    KC_LEFT,            KC_ENTER,           KC_RIGHT,            KC_DOWN,
+        KC_PGDN,            KC_HOME,            KC_KP_ENTER,        KC_END,             KC_PGUP,   KC_UP,    KC_LEFT,            KC_ENTER,           KC_RIGHT,            KC_DOWN,
                                                                                         KC_BSPC,   KC_SPC
     )
 };
