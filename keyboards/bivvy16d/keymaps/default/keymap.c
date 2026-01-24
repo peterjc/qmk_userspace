888dd476043237f76bbb8420db0208185f6bfbb1
Peter J. A. Cock <p.j.a.cock@googlemail.com>
2026-01-24 09:40:59 +0000

    fix

--- keyboards/bivvy16d/keymaps/default/keymap.c
+++ keyboards/bivvy16d/keymaps/default/keymap.c
@@ -4,7 +4,8 @@
 #include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-     /*
+     /* Physical layout close to this:
+      *
       * ┌───┬───┬───┬───┐                   ┌───┬───┬───┬───┐
       * │ Q │ W │ E │ R ├───┐           ┌───┤ U │ I │ O │ P │
       * ├───┼───┼───┼───┤ T │           │ Y ├───┼───┼───┼───┤
@@ -13,16 +14,26 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       * │ Z │ X │ C │ V ├───┘           └───┤ M │ , │ . │ / │
       * └───┴───┴───┴───┘                   └───┴───┴───┴───┘
       *                 ┌───┬───┐  ┌───┬───┐ 
-      *                 │ B │BkS│  │SPC│ N │
+      *      (5-way)    │ B │BkS│  │SPC│ N │     (5-way)
       *                 └───┴───┘  └───┴───┘
       * Home-row mods,
       * ASDF are Left-GUI,    Left-Alt,  Left-Ctrl, Left-Shift
       * JKL; are Right-Shift, Left-Ctrl, Left-Alt,  Left-GUI
+      *
+      * The logical layout here is 5 rows:
+      *
+      * Q 	W 	E 	R 	T 		Y 	U 	I 	O 	P
+      * A 	S 	D 	F 	G 		H 	J 	K 	L 	;
+      * Z 	X 	C 	V 	B 		N 	M 	, 	. 	/
+      * ⬇️	⬅️	⏺️	➡️	⬆️		⬆️	⬅️	⏺️	➡️	⬇️
+      *   	  	  	  	⌫ 		␠
+      *
       */
     [0] = LAYOUT_L16R16(
         KC_Q,               KC_W,               KC_E,               KC_R,               KC_T,      KC_Y,     KC_U,               KC_I,               KC_O,                KC_P,
         MT(MOD_LGUI, KC_A), MT(MOD_LALT, KC_S), MT(MOD_LCTL, KC_D), MT(MOD_LSFT, KC_F), KC_G,      KC_H,     MT(MOD_RSFT, KC_J), MT(MOD_LCTL, KC_K), MT(MOD_LALT, KC_L),  MT(MOD_LGUI, KC_SCLN),
-        KC_Z,               KC_X,               KC_C,               KC_V,                                    KC_M,               KC_COMM,            KC_DOT,              KC_SLSH,
-                                                                    KC_B,               KC_BSPC,   KC_SPC,   KC_N
+        KC_Z,               KC_X,               KC_C,               KC_V,               KC_B,      KC_N,     KC_M,               KC_COMM,            KC_DOT,              KC_SLSH,
+        KC_PGDN,            KC_HOME,            KC_KP_ENTER,        KC_END,             KP_PGUP,   KC_UP,    KC_LEFT,            KC_ENTER,           KC_RIGHT,            KC_DOWN,
+                                                                                        KC_BSPC,   KC_SPC,
     )
 };
